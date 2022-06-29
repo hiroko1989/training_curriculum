724ca87689b48baed345da01c8359e04a4970e31
hiroko1989 <mwam0615@gmail.com>
2022-06-29 22:00:25 +0900

    issue4+6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,15 +8,14 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
-    Plan.create(string: [:plan_params])
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
@@ -36,8 +35,8 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = Date.today.wday
-      if wday_num > 7
+      wday_num = Date.today.wday + x
+      if wday_num >= 7
         wday_num = wday_num -7
       end
 
